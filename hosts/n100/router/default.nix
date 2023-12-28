b8c325c1980da982857c1124ba5f4733a00b7311
lluz55 <lucasluz55@gmail.com>
2023-12-28 07:55:10 -0300

    Remove base.nix and use default instead

--- hosts/n100/router/default.nix
+++ hosts/n100/router/default.nix
@@ -7,4 +7,16 @@
     ./firewall.nix
     ./dnsmasq.nix
   ];
+
+  boot = {
+    kernel = {
+      sysctl = {
+        "net.ipv4.conf.all.forwarding" = true;
+        "net.ipv6.conf.all.forwarding" = true;
+        "net.ipv4.ip_forward" = 1;
+      };
+    };
+  };
+  services.irqbalance.enable = true;
+  services.resolved.enable = false;
 }
