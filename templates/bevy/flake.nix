391098b0c09b5d0f2c8fb85005e41585be36ecad
lluz55 <lucasluz55@gmail.com>
2024-02-11 09:56:08 -0300

    add bevy template

---
+++ templates/bevy/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Bevy";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell =
+          with pkgs; mkShell {
+            nativeBuildInputs = [
+              pkg-config
+            ];
+            buildInputs = [
+              udev
+              alsa-lib
+              vulkan-loader
+              libxkcommon
+              wayland
+            ];
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+          };
+      });
+}
+
