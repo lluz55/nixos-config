3e6acf6290a5818c4d8be150d7dccaeecf2d4028
lluz55 <lucaslluz55@gmail.com>
2024-05-12 09:29:02 -0300

    Zig template working

--- templates/zig/flake.nix
+++ templates/zig/flake.nix
@@ -1,27 +1,53 @@
 {
-  description = "Zig Master";
+  description = "An empty project that uses Zig.";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    zig.url = "github:mitchellh/zig-overlay";
+    zls.url = "github:zigtools/zls";
+
+    # Used for shell.nix
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , zls
+    , ...
+    } @ inputs:
+    let
+      overlays = [
+        # Other overlays
+        (final: prev: {
+          zigpkgs = inputs.zig.packages.${prev.system};
+        })
+      ];
+
+      # Our supported systems are the same supported systems as the Zig binaries
+      systems = builtins.attrNames inputs.zig.packages;
+    in
+    flake-utils.lib.eachSystem systems (
+      system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-          };
-        };
+        pkgs = import nixpkgs { inherit overlays system; };
       in
-      {
-        devShell =
-          with pkgs; mkShell rec {
-            buildInputs = [
-              zig
-              zls
-            ];
-          };
-      });
+      rec {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            zigpkgs.master
+          ];
+          buildInputs = [ zls.packages.${system}.default ];
+        };
+
+        # For compatibility with older versions of the `nix` binary
+        devShell = self.devShells.${system}.default;
+      }
+    );
 }
 
