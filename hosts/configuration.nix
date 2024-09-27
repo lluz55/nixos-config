a4dd90b03a7aa92715b073f81676e39154c1b030
lluz55 <lucasluz55@gmail.com>
2024-09-27 12:02:26 -0300

    update polkit

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -14,7 +14,16 @@
   };
 
   security = {
-    polkit.enable = true;
+    polkit = {
+      enable = true;
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (subject.isInGroup("wheel")) {
+            return polkit.Result.YES;
+          }
+        });
+        '';
+    };
     rtkit.enable = true;
   };
 
