ff287b91b87368fac5f781ac35af4a9b03a42c36
lluz55 <lucasluz55@gmail.com>
2024-10-16 20:45:51 -0300

    fix typo

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -53,7 +53,7 @@
       tarball-ttl = 0;
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
-      extra--substituters = [ "https://cosmic.cachix.org/" "https://nix-community.cachix.org"];
+      extra-substituters = [ "https://cosmic.cachix.org/" "https://nix-community.cachix.org"];
       extra-trusted-substituters = [ "https://cosmic.cachix.org/" "https://nix-community.cachix.org"];
       trusted-public-keys = [
          "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
