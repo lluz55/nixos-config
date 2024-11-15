123b35751422f53f23de42827436d3b83f4b03c2
lluz55 <lucasluz55@gmail.com>
2024-11-15 08:17:17 -0300

    enable podman and podman compose globally

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -43,6 +43,24 @@
     rtkit.enable = true;
   };
 
+  # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  # Useful other development tools
+  environment.systemPackages = with pkgs; [
+    podman 
+    podman-compose # start group of containers for dev
+  ];
+
+
   fonts.packages = with pkgs; [
     fira-code
     font-awesome
