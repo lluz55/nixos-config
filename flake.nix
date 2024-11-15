2c7fec861a1dbb1a4c9279f206bc1d92e5c23bde
lluz55 <lucasluz55@gmail.com>
2024-11-15 08:20:16 -0300

    update formating; cosmic-nix caching and zen browser

--- flake.nix
+++ flake.nix
@@ -1,22 +1,23 @@
 {
   description = "Pers system flake";
-    nixConfig = {
-      accept-flake-config = true;
-      extra-substituters = [
-        "https://cosmic.cachix.org/" 
-        "https://nix-community.cachix.org"
-      ];
-      extra-trusted-substituters = [
-        "https://cosmic.cachix.org/" 
-        "https://nix-community.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
+  nixConfig = {
+    accept-flake-config = true;
+    extra-substituters = [
+      "https://cosmic.cachix.org/"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-substituters = [
+      "https://cosmic.cachix.org/"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.follows = "nixos-cosmic/nixpkgs"; # NOTE: change "nixpkgs" to "nixpkgs-stable" to use stable NixOS release
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
@@ -42,9 +43,9 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     nixos-cosmic = {
       url = "github:lilyinstarlight/nixos-cosmic";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixos-cosmic/nixpkgs";
     };
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
 
     disko = {
       url = "github:nix-community/disko";
@@ -63,143 +64,140 @@
     #};
   };
 
-  outputs =
-    inputs @ { nixpkgs
-    , nixpkgs-unstable
-    , home-manager
-    , flake-parts
-    , nix-direnv
-    , rust-overlay
-    , nixos-cosmic
-    , zen-browser
-    , disko
-    , sops-nix
-      # , nix-ld
-      # , nixos-generators
-    , ...
-    }:
-    let
-      inherit (users) masterUser;
-      inherit (users) karolayne;
-      users = import ./users.nix;
-      system = "x86_64-linux";
-      zen-browser = inputs.zen-browser.packages."${system}".default;
-      # system-aarch64 = "aarch64-linux";
+  outputs = inputs @ {
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    flake-parts,
+    nix-direnv,
+    rust-overlay,
+    nixos-cosmic,
+    zen-browser,
+    disko,
+    sops-nix,
+    # , nix-ld
+    # , nixos-generators
+    ...
+  }: let
+    inherit (users) masterUser;
+    inherit (users) karolayne;
+    users = import ./users.nix;
+    system = "x86_64-linux";
+    zen-browser = inputs.zen-browser.packages."${system}".specific;
+    # system-aarch64 = "aarch64-linux";
 
-      unstable = import nixpkgs-unstable {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      #pkgs-aarch64 = import nixpkgs-unstable {
-      #  system = system-aarch64;
-      #  config.allowUnfree = true;
-      #};
-      inherit (nixpkgs) lib;
-      overlays = [ rust-overlay.overlays.default ];
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      mkSystem = name: cfg:
-        let
-          masterUsername = masterUser.name;
-          additionalUserExists = (cfg.additionalUser or null)
-            != null; # Variable must be boolean
-          additionalUsername = cfg.additionalUser.name;
-        in
-        with lib;
+    unstable = import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    #pkgs-aarch64 = import nixpkgs-unstable {
+    #  system = system-aarch64;
+    #  config.allowUnfree = true;
+    #};
+    inherit (nixpkgs) lib;
+    overlays = [rust-overlay.overlays.default];
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    mkSystem = name: cfg: let
+      masterUsername = masterUser.name;
+      additionalUserExists =
+        (cfg.additionalUser or null)
+        != null; # Variable must be boolean
+      additionalUsername = cfg.additionalUser.name;
+    in
+      with lib;
         nixosSystem
-          {
-            inherit system;
-            specialArgs =
-              {
-                inherit inputs unstable masterUser  nix-direnv zen-browser;
-              }
-              // attrsets.optionalAttrs additionalUserExists { inherit (cfg) additionalUser; };
-            modules =
-              (if (builtins.hasAttr "isVPS" cfg && cfg.isVPS) then [
+        {
+          inherit system;
+          specialArgs =
+            {
+              inherit inputs unstable masterUser nix-direnv zen-browser;
+            }
+            // attrsets.optionalAttrs additionalUserExists {inherit (cfg) additionalUser;};
+          modules =
+            (
+              if (builtins.hasAttr "isVPS" cfg && cfg.isVPS)
+              then [
                 # VPS only configuration
                 ./hosts/vps-server
               ]
-                else
-             [
+              else [
                 ./modules
                 ./hosts/configuration.nix
                 ./hosts/${name}
                 masterUser.user
                 home-manager.nixosModules.home-manager
                 # nix-ld.nixosModules.nix-ld
-                ({
+                {
                   nixpkgs.overlays = overlays;
-                })
+                }
                 nixos-cosmic.nixosModules.default
                 {
                   home-manager = {
                     useGlobalPkgs = true;
                     useUserPackages = true;
-                    extraSpecialArgs = { inherit pkgs unstable masterUser nix-direnv inputs; };
+                    extraSpecialArgs = {inherit pkgs unstable masterUser nix-direnv inputs;};
                     users =
                       {
                         # Load HM configuration for main user
-                        "${masterUsername}".imports = [ ./home/${masterUsername}.nix ];
+                        "${masterUsername}".imports = [./home/${masterUsername}.nix];
                       }
                       // attrsets.optionalAttrs additionalUserExists {
                         # Load HM configuration for additional user
-                        "${additionalUsername}".imports = [ ./home/${additionalUsername}.nix ];
+                        "${additionalUsername}".imports = [./home/${additionalUsername}.nix];
                       };
                   };
                 }
-              ])
-              
-              # In case additional modules was passed
-              ++ (cfg.modules or [ ])
-              # Details from additional user
-              ++ (
-                if additionalUserExists
-                then [ cfg.additionalUser.user ]
-                else [ ]
-              );
-          };
-      # All hosts
-      hosts = {
-        n100 = { 
-          modules = [ sops-nix.nixosModules.sops ];
-          };
-        b450 = {
-          modules = [ sops-nix.nixosModules.sops ];
+              ]
+            )
+            # In case additional modules was passed
+            ++ (cfg.modules or [])
+            # Details from additional user
+            ++ (
+              if additionalUserExists
+              then [cfg.additionalUser.user]
+              else []
+            );
         };
-        gl62m = {
-          # TODO: Maybe convert to a List
-          additionalUser = karolayne;
-        };
-        vps-server = {
-          modules = [ disko.nixosModules.disko ];
-          isVPS = true;
-        };
+    # All hosts
+    hosts = {
+      n100 = {
+        modules = [sops-nix.nixosModules.sops];
       };
-    in flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+      b450 = {
+        modules = [sops-nix.nixosModules.sops];
+      };
+      gl62m = {
+        # TODO: Maybe convert to a List
+        additionalUser = karolayne;
+      };
+      vps-server = {
+        modules = [disko.nixosModules.disko];
+        isVPS = true;
+      };
+    };
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
       flake = {
         templates = {
           flutter = {
             path = ./templates/flutter;
-            description =
-              "nix flake new -t github:lluz55/nixos-config#flutter <directory>";
+            description = "nix flake new -t github:lluz55/nixos-config#flutter <directory>";
           };
           zig = {
             path = ./templates/zig;
-            description =
-              "nix flake new -t github:lluz55/nixos-config#zig <directory>";
+            description = "nix flake new -t github:lluz55/nixos-config#zig <directory>";
           };
           bevy = {
             path = ./templates/bevy;
-            description =
-              "nix flake new -t github:lluz55/nixos-config#bevy <directory>";
+            description = "nix flake new -t github:lluz55/nixos-config#bevy <directory>";
           };
           godot_rust = {
             path = ./templates/godot_rust;
-            description =
-              "nix flake new -t github:lluz55/nixos-config#godot_rust <directory>";
+            description = "nix flake new -t github:lluz55/nixos-config#godot_rust <directory>";
           };
         };
         #packages."x86_64-linux" = {
@@ -208,7 +206,7 @@
         #    format = "iso";
         #    modules = [ ./modules/aarch64-linux-base.nix ];
         #  };
-          #};
+        #};
         #packages.${system}.neovim = neovim-flake.packages.${system}.maximal;
         nixosConfigurations = lib.mapAttrs mkSystem hosts;
       };
