48236fd74197f46f2caf65c64b150c5b049b250f
lluz55 <lucasluz55@gmail.com>
2025-11-06 19:14:39 -0300

    comment out blocky

--- modules/tools/blocky.nix
+++ modules/tools/blocky.nix
@@ -1,7 +1,7 @@
 { pkgs, config, masterUser, ... }:
 {
   services.blocky = {
-    enable = true;
+    enable = false;
     settings = {
       ports.dns = 53; # Port for incoming DNS Queries.
       upstreams.groups.default = [
@@ -16,17 +16,17 @@
       blocking = {
         blackLists = {
           #Adblocking
-          ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+          ads = [ "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" ];
           #Another filter for blocking adult sites
-          adult = ["https://blocklistproject.github.io/Lists/porn.txt"];
+          adult = [ "https://blocklistproject.github.io/Lists/porn.txt" ];
           #You can add additional categories
         };
         #Configure what block categories are used
         clientGroupsBlock = {
           default = [ "ads" ];
-          kids-ipad = ["ads" "adult"];
+          kids-ipad = [ "ads" "adult" ];
         };
       };
     };
-  }; 
+  };
 }
