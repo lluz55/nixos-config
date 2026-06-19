9150962ad4af2d0a3152bfc3c1f5d290ea667613
lluz55 <lucasluz55@gmail.com>
2026-06-19 11:30:23 -0300

    feat: add cosmic binary cache to speed up builds

--- flake.nix
+++ flake.nix
@@ -4,12 +4,15 @@
     accept-flake-config = true;
     extra-substituters = [
       "https://nix-community.cachix.org"
+      "https://cosmic.cachix.org"
     ];
     extra-trusted-substituters = [
       "https://nix-community.cachix.org"
+      "https://cosmic.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
     ];
   };
   inputs = {
--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -130,10 +130,17 @@
       # tarball-ttl = 0;
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
-      extra-substituters = [ "https://nix-community.cachix.org" ];
-      extra-trusted-substituters = [ "https://nix-community.cachix.org" ];
+      extra-substituters = [
+        "https://nix-community.cachix.org"
+        "https://cosmic.cachix.org"
+      ];
+      extra-trusted-substituters = [
+        "https://nix-community.cachix.org"
+        "https://cosmic.cachix.org"
+      ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       ];
     };
     gc = {
